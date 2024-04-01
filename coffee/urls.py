411506452d6bea939bb0f53c6631c6066cffbf94
EmsonFranca <esf12364@gmail.com>
2024-04-01 15:19:46 -0300

    refatorando

--- coffee/urls.py
+++ coffee/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from rest_framework.authtoken import views
-from accounts.Api.views import AccountListCreateView
 from recipes.Api.views import RecipeListCreateView, IngredientListCreateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -16,6 +15,7 @@ router.register("api/ingredient", IngredientListCreateView, basename="ingredient
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('user/', include('user.api.urls')),
     path("api/token-auth/", views.obtain_auth_token),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
